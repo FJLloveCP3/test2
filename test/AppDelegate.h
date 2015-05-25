b04eb3e7787d4a782e28da20f183f1beeff021b4
冯敬磊 <fengjinglei@fengjingleideMacBook-Pro.local>
2015-05-25 21:10:34 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 冯敬磊 on 15/5/25.
+//  Copyright (c) 2015年 冯敬磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 冯敬磊 on 15/5/25.
+//  Copyright (c) 2015年 冯敬磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
